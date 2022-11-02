70b4f0168b2691a5d1da21370a9ad39ac3a4ae8d
dylanlazy <116140781+dylanlazy@users.noreply.github.com>
2022-11-02 11:52:30 +0700

    Add files via upload

---
+++ Make_a_list.py
@@ -0,0 +1,5 @@
+colorlist = ["red", "yellow" , "blue" , "green"]
+colorlist.append("orange")  
+color = input("Enter the color: ")
+colorlist.append(color)
+print(colorlist)
---
+++ forloop.py
@@ -0,0 +1,6 @@
+colorlist = []
+
+for i in range(5): #5 loops to add 5 colors
+    color = input("Enter the color: ") #variable to enter input
+    colorlist.append(color)
+print(colorlist)

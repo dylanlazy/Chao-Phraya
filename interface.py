8fa62d6c08210a0eaaf5e7c828051124bec16cb0
dylanlazy <116140781+dylanlazy@users.noreply.github.com>
2023-01-11 12:09:01 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -12,15 +12,38 @@ while choice != 'X':
     print("TEAM MANAGER")
     print("=================")
     print("This program will help you manage your team")
-    print("/n")
+    print("\n")
     print("A: Append a value")
     print("B: Print the teamlist")
+    print("C: Print an Element")
+    print("D: Delete an Element")
+    print("E: Edit an Element")
+    print("F: Sort the list")
     print("X: Exit program")
     choice = input("Enter your choice: ")
     if choice == 'A':
         name = input("Enter a name: ")
         teamlist.append(name)
-print(teamlist)        
+    if choice =='B':
+        print(teamlist)    
+    if choice == 'C':
+        i = int(input("Which list item do you want to print: "))    
+        print(teamlist[i-1])
+    if choice == 'D':
+        delete = int(input("Which do you want to delete: "))
+        del(teamlist[delete-1])
+    if choice == 'E':
+        edit = int(input("Which do you want to edit: "))
+        teamlist[edit-1]  = input("What do you want to change it to: ")
+        print(teamlist)
+    if choice == 'F':
+       teamlist.sort()    
+        
+
+
+
+
+
 
 
  
